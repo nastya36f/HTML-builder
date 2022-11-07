b3643d59c34b54ac4e78c998ddd103fae42825ba
Nastya Filippova <nastya.filippova87@gmail.com>
2022-11-07 18:28:40 +0300

    feat: add solution 01-read-file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require ('fs');
+const path = require ('path');
 
+
+function copyDir () {
+    fs.readdir(__dirname, (err, files) => {
+        if (err) {
+            throw err;
+        } else {
+            if (files.includes('files-copy')) {
+                console.log('Папка уже существует');
+            } else {
+               fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: false}, err => {
+                if (err) {
+                    throw err;
+                }
+               })
+            }
+        }
+        
+    })
+}
+
+copyDir();
